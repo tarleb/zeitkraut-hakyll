efa6744b0ea7a47116c1fbc4ece878ed2ba21ae8
Albert Krewinkel <tarleb@moltkeplatz.de>
2013-09-15 16:48:16 +0200

    Animated logos

--- scripts/zeitlinse.js
+++ scripts/zeitlinse.js
@@ -1,5 +1,29 @@
+var clock = function() {
+  var sDial = document.getElementsByClassName("seconds")[0];
+  var mDial = document.getElementsByClassName("minutes")[0];
+  var hDial = document.getElementsByClassName("hours")[0];
+
+  function moveDials(hours, minutes, seconds) {
+    hDeg = (360 / 12) * hours;
+    mDeg = (360 / 60) * minutes;
+    sDeg = (360 / 60) * seconds;
+    sDial.setAttribute("transform", "rotate("+sDeg+")");
+    mDial.setAttribute("transform", "rotate("+mDeg+")");
+    hDial.setAttribute("transform", "rotate("+hDeg+")");
+  }
+
+  function update() {
+    var currentTime = new Date();
+    moveDials(currentTime.getHours(), currentTime.getMinutes(), currentTime.getSeconds());
+  };
+  update();
+}
+
 // convert obfuscated mail addresses into clickable "mailto:" links
 jQuery("document").ready(function($) {
+  // Transform logos into working clocks
+  // setInterval(clock, 50);
+
   var mailAddressClass = "obfuscated-mail-address";
   $("."+mailAddressClass).each(function() {
     if (!$(this).hasClass("dont-touch")) {

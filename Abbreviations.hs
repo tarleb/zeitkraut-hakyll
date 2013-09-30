44eec17afd824ce577dfbb13d6878be95ca38ff2
Albert Krewinkel <tarleb@moltkeplatz.de>
2013-09-30 14:13:42 +0200

    Get abbreviations module to compile

--- Abbreviations.hs
+++ Abbreviations.hs
@@ -1,24 +1,24 @@
 module Abbreviation
-       (replaceAbbreviations)
+       (markAbbreviations)
 where
 
 import           Prelude
-import           Map (Map)
-import qualified Map as M
+import           Data.Map (Map)
+import qualified Data.Map as M
 import           Hakyll
 
 markAbbreviations :: (Map String String) -> String -> String
 markAbbreviations abbrMap text =
-  replaceAll "%[a-zA-Z0-9_]*" replacement
+  replaceAll "%[a-zA-Z0-9_]*" replacement text
     where replacement matched = case M.lookup (tail matched) abbrMap of
             Nothing -> matched
-            Just a -> "<abbr title=\"" ++ a ++ "\">" ++ matched ++ "</abbr>"
+            Just a -> "<abbr title=\"" ++ a ++ "\">" ++ (tail matched) ++ "</abbr>"
 
 testAbbrMap :: Map String String
 testAbbrMap = M.fromList
               [ ("test", "Just a stupid simple test")
               ]
 
-testText = "Hello, World, this %test is really stupid."
+testText = "Hello, World!  This %test is really stupid."
 
 main = print $ markAbbreviations testAbbrMap testText
